a36346da81db82ac31b92b5d9be69a3e1275e966
abhiars777 <abhiars777@gmail.com>
2024-09-14 16:48:30 +0530

    Check Point 2

---
+++ src/main/java/com/eventmanager/controllers/EventController.java
@@ -0,0 +1,58 @@
+package com.eventmanager.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.yaml.snakeyaml.events.Event;
+
+import com.eventmanager.models.Attendee;
+import com.eventmanager.services.EventService;
+
+@Controller
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+    
+    @GetMapping("/")
+    public String listEvents(Model model) {
+        //model.addAttribute("events", eventService.listEvents());
+        return "events";
+    }
+
+    @GetMapping("/event/{id}")
+    public String viewEvent(@PathVariable Long id, Model model) {
+        //Event event = eventService.getEvent(id);
+        //model.addAttribute("event", event);
+        return "event-details";  // Refers to 'event-details.html'
+    }
+
+    @GetMapping("/create-event")
+    public String createEventForm(Model model) {
+        //model.addAttribute("event", new Event());
+        return "create-event";  // Refers to 'create-event.html'
+    }
+
+    @PostMapping("/create-event")
+    public String createEvent(@ModelAttribute Event event) {
+        //eventService.createEvent(event);
+        return "redirect:/";
+    }
+
+    @PostMapping("/event/{id}/add-attendee")
+    public String addAttendee(@PathVariable Long id, @ModelAttribute Attendee attendee) {
+        //eventService.addAttendee(id, attendee);
+        return "redirect:/event/" + id;
+    }
+
+    @PostMapping("/event/{id}/reminder")
+    public String sendReminder(@PathVariable Long id) {
+        //eventService.sendReminder(id);
+        return "redirect:/event/" + id;
+    }
+
+}
---
+++ src/main/java/com/eventmanager/models/Event.java
@@ -0,0 +1,34 @@
+package com.eventmanager.models;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime dateTime;
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    private List<Attendee> attendees = new ArrayList<>();
+
+}
